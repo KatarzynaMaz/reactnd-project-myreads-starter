21dec7e4b48296cc10309575533e97cdb4fb0da9
Katarzyna Mazur <mazur66@hotmail.com>
2018-09-14 13:23:33 -0400

    Added state (list of books) in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+
 import SearchPage from './SearchPage';
 import MainPage from './MainPage';
 
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    })
+  }
  
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -15,6 +25,7 @@ class BooksApp extends React.Component {
      */
    
   render() {
+   console.log(this.state.books);
     return (
       <div className="app">
       <MainPage/>
