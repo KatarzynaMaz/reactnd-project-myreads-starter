16d8a6bea743f7e9b3a540909e8b3a83a5081dd4
Katarzyna Mazur <mazur66@hotmail.com>
2018-09-15 15:28:32 -0400

    Corrected the link to image

--- src/Book.js
+++ src/Book.js
@@ -9,7 +9,7 @@ class Book extends Component {
             <div className="book">
                 <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 
-                `url({book.imageLinks.thumbnail})`
+                `url(${book.imageLinks.thumbnail})`
             }}></div>
                 <div className="book-shelf-changer">
                     <select>
