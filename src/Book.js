5f61a5c5b86de2511bf861462646770617d41399
Katarzyna Mazur <mazur66@hotmail.com>
2018-09-15 14:17:59 -0400

    Added book information to the Book Component

--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react'
 
 class Book extends Component {
+        
     render() {
+       let book = this.props.book;
+        
         return(
             <div className="book">
                 <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: '' }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 
+                `url({book.imageLinks.thumbnail})`
+            }}></div>
                 <div className="book-shelf-changer">
                     <select>
                     <option value="move" disabled>Move to...</option>
@@ -16,8 +21,8 @@ class Book extends Component {
                     </select>
                 </div>
                 </div>
-                <div className="book-title"></div>
-                <div className="book-authors"></div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
             </div>
         );
     }
