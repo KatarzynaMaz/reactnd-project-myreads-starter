6a454e0b937a81822992e4a4582a700f4c1e09bf
Katarzyna Mazur <mazur66@hotmail.com>
2018-09-17 22:44:21 -0400

    Spelling correction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-rounter-dom'
+import {BrowserRouter} from 'react-router-dom'
 import App from './App'
 import './index.css'
 
